c133633fe32809f496fcd9fae415b7911c9eaf1b
Jason <121495300+jason21715@users.noreply.github.com>
2023-03-03 00:17:00 -0800

    Add library skeleton

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-let myLibrary = ["Percy Jackson", "Percy", "James"];
-
-function addBookToLibrary(...args) {
-  myLibrary += args;
-}
-
-function Books() {
-  for (const i of myLibrary) {
-    console.log(i);
+class Book {
+  constructor(title, author, pages, completed) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.completed = completed;
   }
 }
 
-Books();
+let myLibrary = [];
+
+function Books() {
+  for (const i of myLibrary) {
+    console.log(i.title);
+    console.log(i.author);
+    console.log(i.pages);
+    console.log(i.completed);
+  }
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
 const newBookButton = document.getElementById("newBook");
+const addBookButton = document.getElementById("addBook");
+const bookTitle = document.getElementById("bookTitle");
+const bookAuthor = document.getElementById("bookAuthor");
+const bookPages = document.getElementById("bookPages");
+const bookCompletion = document.getElementById("bookCompletion");
+
 const toggleNewBookForm = document.getElementById("newBookForm");
-newBookButton.addEventListener("click", function (e) {
+
+newBookButton.addEventListener("click", () => {
+  newBookButton.style.display = "none";
   toggleNewBookForm.style.display = "block";
-  e.preventDefault;
 });
+
+addBookButton.addEventListener("click", function (e) {
+  const book1 = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookCompletion.value
+  );
+
+  addBookToLibrary(book1);
+  console.log(book1);
+  Books();
+  e.preventDefault();
+});
+
+Books();
