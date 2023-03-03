50e98de00e658d751b60b06a1f4ce3a7f54a54e5
Jason <121495300+jason21715@users.noreply.github.com>
2023-03-03 00:48:43 -0800

    Update add book function

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const book1Title = document.getElementsByClassName("book1Title");
+const book1Author = document.getElementsByClassName("book1Title");
+const book1Pages = document.getElementsByClassName("book1Title");
+const book1Completion = document.getElementsByClassName("book1Title");
+
+const newBookButton = document.getElementById("newBook");
+const addBookButton = document.getElementById("addBook");
+const bookTitle = document.getElementById("bookTitle");
+const bookAuthor = document.getElementById("bookAuthor");
+const bookPages = document.getElementById("bookPages");
+const bookCompletion = document.getElementById("bookCompletion");
+const toggleNewBookForm = document.getElementById("newBookForm");
+
 class Book {
   constructor(title, author, pages, completed) {
     this.title = title;
@@ -11,25 +24,25 @@ let myLibrary = [];
 
 function Books() {
   for (const i of myLibrary) {
-    console.log(i.title);
-    console.log(i.author);
-    console.log(i.pages);
-    console.log(i.completed);
+    const book1TitleElement = document.querySelector(".book1Title");
+    const book1AuthorElement = document.querySelector(".book1Author");
+    const book1PagesElement = document.querySelector(".book1Pages");
+    const book1CompletionElement = document.querySelector(".book1Completion");
+
+    book1TitleElement.textContent = `Book #1 TITLE: ${i.title}`;
+    book1AuthorElement.textContent = `AUTHOR: ${i.author}`;
+    book1PagesElement.textContent = `PAGES: ${i.pages}`;
+    if (i.completed === "on") {
+      book1CompletionElement.textContent = "STATUS: COMPLETED";
+    } else if (i.completed === "off") {
+      book1CompletionElement.textContent = "STATUS: NOT COMPLETED";
+    }
   }
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
-
-const newBookButton = document.getElementById("newBook");
-const addBookButton = document.getElementById("addBook");
-const bookTitle = document.getElementById("bookTitle");
-const bookAuthor = document.getElementById("bookAuthor");
-const bookPages = document.getElementById("bookPages");
-const bookCompletion = document.getElementById("bookCompletion");
-
-const toggleNewBookForm = document.getElementById("newBookForm");
 
 newBookButton.addEventListener("click", () => {
   newBookButton.style.display = "none";
@@ -45,8 +58,11 @@ addBookButton.addEventListener("click", function (e) {
   );
 
   addBookToLibrary(book1);
-  console.log(book1);
   Books();
+
+  addBookButton.style.display = "none";
+  toggleNewBookForm.style.display = "none";
+
   e.preventDefault();
 });
 
